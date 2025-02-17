07bb4a3baa4d4b887003e88d7f90ac4daa5f8ca6
Lovepreet Singh <singhlovepreet05198@gmail.com>
2025-02-17 16:08:13 +0530

    Update articles.js

--- articles/articles.js
+++ articles/articles.js
@@ -2,23 +2,22 @@
 fetch("https://brightmindzhub.github.io/Brightmindz-/articles.json")
 .then(response => response.json())
 .then(data => {
-    // Show first 2 articles as related
-    let relatedArticles = data.slice(0, 2);
+    // Show the latest article (first in the list)
+    let latestArticle = data[0];
 
     let container = document.getElementById("related-articles-container");
-    relatedArticles.forEach(post => {
-        let postElement = document.createElement("div");
-        postElement.classList.add("post-preview");
 
-        postElement.innerHTML = `
-            <div class="post-button" onclick="window.location.href='${post.url}'">
-                <h3>${post.title}</h3>
-                <p>${post.preview}</p>
-                <span class="read-more">Read More</span>
-            </div>
-        `;
+    let postElement = document.createElement("div");
+    postElement.classList.add("post-preview");
 
-        container.appendChild(postElement);
-    });
+    postElement.innerHTML = `
+        <div class="post-button" onclick="window.location.href='${latestArticle.url}'">
+            <h3>${latestArticle.title}</h3>
+            <p>${latestArticle.preview}</p>
+            <span class="read-more">Read More</span>
+        </div>
+    `;
+
+    container.appendChild(postElement);
 })
-.catch(error => console.error("Error loading related articles:", error));
+.catch(error => console.error("Error loading the latest article:", error));
