91c171cf1de2c8f47c303e1cc399b44fd9316d05
Lovepreet Singh <singhlovepreet05198@gmail.com>
2025-02-28 16:16:55 +0530

    Update generate_sitemap.py

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -3,13 +3,46 @@ import json
 from datetime import datetime
 
 JSON_FILE = "articles/preview.json"
-
-DOMAIN = "https://Rgthinks.top"
+DOMAIN = "https://rgthinks.top"
 
 sitemap_content = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 """
 
+# Static pages manually added
+static_pages = [
+    f"{DOMAIN}/",
+    f"{DOMAIN}/pages/about-us",
+    f"{DOMAIN}/pages/contact-us",
+    f"{DOMAIN}/pages/terms",
+    f"{DOMAIN}/pages/copyright",
+    f"{DOMAIN}/pages/privacy",
+    f"{DOMAIN}/categories/category.html?category=Technology",
+    f"{DOMAIN}/categories/category.html?category=Artificial%20Intelligence",
+    f"{DOMAIN}/categories/category.html?category=Programming%20and%20Web%20Development",
+    f"{DOMAIN}/categories/category.html?category=Cybersecurity",
+    f"{DOMAIN}/categories/category.html?category=Finance%20and%20Cryptocurrency",
+    f"{DOMAIN}/categories/category.html?category=Health%20and%20Wellness",
+    f"{DOMAIN}/categories/category.html?category=Education%20and%20E-learning",
+    f"{DOMAIN}/categories/category.html?category=Science%20and%20Space",
+    f"{DOMAIN}/categories/category.html?category=Entertainment%20and%20Movies",
+    f"{DOMAIN}/categories/category.html?category=Gaming%20and%20Esports",
+    f"{DOMAIN}/search/search.html",
+]
+
+# Add static pages to sitemap
+for url in static_pages:
+    lastmod = datetime.today().strftime('%Y-%m-%d')
+    sitemap_content += f"""
+    <url>
+        <loc>{url}</loc>
+        <lastmod>{lastmod}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.7</priority>
+    </url>
+    """
+
+# Add articles dynamically from JSON file
 with open(JSON_FILE, "r", encoding="utf-8") as file:
     articles = json.load(file)
 
@@ -20,12 +53,12 @@ for article in articles:
     <url>
         <loc>{url}</loc>
         <lastmod>{lastmod}</lastmod>
-        <changefreq>weekly</changefreq>
+        <changefreq=weekly</changefreq>
         <priority>0.8</priority>
     </url>
     """
 
-sitemap_content += "</urlset>"
+sitemap_content += "\n</urlset>"
 
 with open("sitemap.xml", "w", encoding="utf-8") as f:
     f.write(sitemap_content)
